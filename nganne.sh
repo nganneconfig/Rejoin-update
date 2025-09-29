b0abb58ac28e5bc4d054856515d5d19c97561aad
nganneconfig <nganvps2008@gmail.com>
2025-09-29 18:29:44 +0700

    Update nganne.sh

--- nganne.sh
+++ nganne.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 cd
-
-# Setup storage
 if [ -e "/data/data/com.termux/files/home/storage" ]; then
-    rm -rf /data/data/com.termux/files/home/storage
+rm -rf /data/data/com.termux/files/home/storage
 fi
 termux-setup-storage
-
-# Update & change repo
 yes | pkg update
-. <(curl -fsSL https://raw.githubusercontent.com/nganneconfig/rejoinshouko/refs/heads/main/termux-change-repo.sh)
+. <(curl https://raw.githubusercontent.com/nganneconfig/rejoinshouko/refs/heads/main/termux-change-repo.sh)
 yes | pkg upgrade
-
-# Install base packages
-yes | pkg install python -y
-yes | pkg install python-pip -y
-yes | pkg install git -y
-yes | pkg install curl -y
-
-# Python dependencies
-pip install --upgrade pip
-pip install requests rich prettytable pytz psutil aiohttp pyfiglet
-
-# Wake lock để giữ cho Termux không bị sleep
-termux-wake-lock
-
-# Tải tool về thư mục chính
+yes | pkg i python
+yes | pkg i python-pip
+pip install requests rich prettytable pytz
+export CFLAGS="-Wno-error=implicit-function-declaration"
+pip install psutil
 curl -Ls "https://raw.githubusercontent.com/nganneconfig/Rejoin-update/refs/heads/main/rejoin_webhook.py" -o /sdcard/Download/rejoin_webhook.py
